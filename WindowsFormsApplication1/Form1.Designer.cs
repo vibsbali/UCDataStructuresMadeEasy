1cb696105e5c8a437b6a9538b8dc12b6a40e937d
Vaibhav Bali <vbali@internode.on.net>
2016-07-15 09:19:03 +1000

    init

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class FrmTextEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstAutoComplete = new System.Windows.Forms.ListBox();
+            this.txtEditor = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lstAutoComplete
+            // 
+            this.lstAutoComplete.FormattingEnabled = true;
+            this.lstAutoComplete.Location = new System.Drawing.Point(12, 281);
+            this.lstAutoComplete.Name = "lstAutoComplete";
+            this.lstAutoComplete.Size = new System.Drawing.Size(209, 82);
+            this.lstAutoComplete.TabIndex = 1;
+            // 
+            // txtEditor
+            // 
+            this.txtEditor.AcceptsReturn = true;
+            this.txtEditor.AcceptsTab = true;
+            this.txtEditor.Location = new System.Drawing.Point(12, 12);
+            this.txtEditor.Multiline = true;
+            this.txtEditor.Name = "txtEditor";
+            this.txtEditor.Size = new System.Drawing.Size(579, 263);
+            this.txtEditor.TabIndex = 2;
+            this.txtEditor.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txtEditor.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.richTextBox1_KeyPress);
+            // 
+            // FrmTextEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(615, 391);
+            this.Controls.Add(this.txtEditor);
+            this.Controls.Add(this.lstAutoComplete);
+            this.Name = "FrmTextEditor";
+            this.Text = "Word";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox lstAutoComplete;
+        private System.Windows.Forms.TextBox txtEditor;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class FrmTextEditor : Form
+    {
+        StringBuilder word = new StringBuilder();
+
+        public FrmTextEditor()
+        {
+            InitializeComponent();
+        }
+        
+        private void richTextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar != ' ')
+            {
+                word.Append(e.KeyChar);
+            }
+            else
+            {
+                MessageBox.Show(word.ToString());
+                word.Clear();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
